2060042e50a894b6ffb83192e1d0ccd807034698
Ramona <rare2400@student.miun.com>
2025-02-25 13:07:54 +0100

    flyttar funktion för start av animering

--- src/js/main.js
+++ src/js/main.js
@@ -25,16 +25,3 @@ function toggleMenu() {
        navMenuEl.style.display = "none";
    }
 }
-
-//Function to start/stop animation
-//Elements
-const animationBtn = document.querySelector(".a-btn");
-const aniContainer = document.querySelector(".golf-animation")
-
-//Eventlistener
-animationBtn.addEventListener("click", toggleAnimation);
-
-//Function to start/stop animation
-function toggleAnimation() {
-    aniContainer.classList.toggle("animate");
-}
